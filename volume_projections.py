33e3a229c1eae097df7bd5bad21489e4c092bc94
raquel adaia sandoval ortega <raqueladaia@gmail.com>
2024-01-10 11:42:36 -0500

    add gitignore

--- volume_projections.py
+++ volume_projections.py
@@ -1,12 +1,14 @@
 import numpy as np
 from PIL import Image
 import os
+from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
+
 
 def open_image_from_desktop(image_name):
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
     image_path = os.path.join(desktop_path, image_name)
-    image = Image.open(image_path)
+    image = Image.open(image_path).convert('L')  # Convert image to grayscale
     return image
 
 
@@ -25,7 +27,8 @@ if __name__ == '__main__':
     img_name = '747_PAG_20x_crop.tif'
 
     img = open_image_from_desktop(img_name)
-    img_bin = binarize_image(img)
+    thr = threshold_otsu(np.array(img))
+    img_bin = binarize_image(img, thr)
 
     fig, ax = plt.subplots(2, 1)
     ax[0].image(img)
