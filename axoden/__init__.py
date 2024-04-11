100a6ba994452f2454f8834bf41790159402f870
raquel adaia sandoval ortega <raqueladaia@gmail.com>
2024-04-11 15:17:08 -0400

    create axoden dir to use as module; flit setup and configuration

---
+++ axoden/__init__.py
@@ -0,0 +1,16 @@
+"""
+The `axoden` module provides functionality for performing various operations related to quantification of axonal projections.
+
+This module contains functions that can be used to quantify axons from images.
+
+Usage:
+    import axoden
+
+    # Initiate GUI - work in progress
+    my_axoden = axoden.initiate_gui()
+
+"""
+
+__version__ = "0.1.1"    
+
+# Rest of the code...
